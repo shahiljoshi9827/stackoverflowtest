857d9136236a20d140671bf25555cdb58275d004
shahiljoshi9827 <shahiljoshi98@gmail.com>
2022-06-05 23:48:16 +0530

    fix: deleted user app

--- stackoverflowtest/settings.py
+++ stackoverflowtest/settings.py
@@ -35,9 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'user',
     'QA',
+    'rest_framework',
     'rest_framework.authtoken',
 
 ]
