2ff41507154c7bb1e8faf24f985fd07ddcf530b8
shahiljoshi9827 <shahiljoshi98@gmail.com>
2022-06-05 12:09:41 +0530

    feat:project setup,token auth,QA models

---
+++ QA/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+# Create your models here.
+class Tags(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+
+class QuestionTag(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tags, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.tag.name
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.answer
+
+
